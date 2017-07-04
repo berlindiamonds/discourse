facc16f970538a3e24db17fcb2537820b33efcea
Jen <jvarelalijo@gmail.com>
2017-07-04 17:04:56 +0200

    locations plugin & customised plugin

---
+++ plugins/hello/plugin.rb
@@ -0,0 +1,8 @@
+# name: plugin-tutorial
+# about: learning how to build a plugin!
+# version: 0.1
+# authors: Kaja & Jen
+# url: https://github.com/Mittineague/discourse-plugin-outlet-locations.git
+
+enabled_site_setting :awesomeness_enabled
+enabled_site_setting :details_enabled
